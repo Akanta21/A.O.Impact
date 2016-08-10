0ea3c4d5054be7123dd49c7e0a35cee39a8a39ea
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-08-10 15:01:17 +0800

    res.header('Access-Control-Allow-Methods', '... PATCH')

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, User-Email, Auth-Token')
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT, PATCH')
   next()
 })
 
