bb196a3b834c1ab542b5ad9fc35cf6c649abeba4
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-11-10 00:51:48 +0800

    updated stripe test

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -1,7 +1,7 @@
 const Order = require('../models/order')
 
 // stripe
-const stripe = require('stripe')('sk_test_vtR8t4Rbfp4ZhGgL5xSaI1xa')
+const stripe = require('stripe')('sk_test_IwdAjYinHaOwGDGscpXEu5pO')
 
 // create new order
 function newOrder (req, res) {
